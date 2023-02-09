c274f3110548bf4d32f24d741c9ed8b032fe13b2
tpence06 <tpence06@hotmail.com>
2023-02-09 14:22:59 -0700

    add photos, background, social media bar

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+  
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
+      }
+    }
+  });
+  
